97ac8fe2745a932788b0c0285e1ea0079c69da1f
emirrozerr <emirrozerr@hotmail.com>
2024-04-10 19:32:29 +0200

    First commit

---
+++ src/main/java/org/example/awtproject/AwtProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.awtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwtProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/awtproject/AwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
