0d0ce6275c2f8cb7331b4ca79e71a40e4d2db81d
emirrozerr <emirrozerr@hotmail.com>
2024-05-10 14:54:48 +0200

    fix

--- src/frontend/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 3000,
-    proxy: 'http://localhost:8080'
-  },
-})
